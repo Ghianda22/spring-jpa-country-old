0bb0fc388dd64ffcdd5f4634b0b4417c62398e94
Valentina <valelore98@gmail.com>
2022-10-18 15:37:27 +0200

    first commit

---
+++ src/test/java/com/country/jpa/spring/SpringJpaCountryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.country.jpa.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaCountryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
